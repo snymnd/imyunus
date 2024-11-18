62195a34fb3d9628736cd2b52c0812a7c281aa6f
snymnd <yunusenvire@gmail.com>
2024-11-18 21:26:37 +0700

    feat: add svgr

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,25 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts'],
+
+  // SVGR
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            typescript: true,
+            icon: true,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
